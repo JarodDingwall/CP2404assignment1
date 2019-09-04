9e3fc11fa489d65eec9ec475a5aa39cd4624a794
JarodDingwall <jarod.dingwall@my.jcu.edu.au>
2019-09-04 12:54:30 +1000

    Add roadNumber and roadLength variables to Road class

--- src/part1/Main.java
+++ src/part1/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Car car = new Car(0,1);
-
+        Road road = new Road(4,1);
+    System.out.println(car.getPosition());
     }
 }
--- src/part1/Road.java
+++ src/part1/Road.java
@@ -1,6 +1,29 @@
 package part1;
 
 public class Road {
+    private int roadLength;
+    private int roadNumber;
+
+    public Road(int roadLength, int roadNumber){
+
+    }
+
+    public void setRoadLength(int value){
+        roadLength = value;
+    }
+
+    public int getRoadLength()  {
+        return roadLength;
+    }
+
+    public void setRoadNumber(int value){
+        roadNumber = value;
+    }
+
+    public int getRoadNumber()  {
+        return roadNumber;
+    }
+
 
 
 }
