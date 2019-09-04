039bdf445cc1c2393017a4d52bf10f3dfe108ff5
JarodDingwall <jarod.dingwall@my.jcu.edu.au>
2019-09-04 12:38:15 +1000

    Add carLength int value and initialise Car class in Main

---
+++ src/part1/Car.java
@@ -0,0 +1,28 @@
+package part1;
+
+public class Car {
+    private int position;
+    private int carLength;
+
+    public Car(int position, int carLength){
+
+    }
+
+    public void setPosition(int value)  {
+        position = value;
+    }
+
+    public int getPosition()    {
+        return position;
+    }
+
+    public void setCarLength(int value) {
+        carLength = value;
+    }
+
+    public int getCarLength()   {
+        return carLength;
+    }
+
+
+}
